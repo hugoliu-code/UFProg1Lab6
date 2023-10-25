2ef5928e2b08aba751a6ad64af206a97ac1bede1
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2023-10-25 19:43:50 -0400

    created file

---
+++ main.py
@@ -0,0 +1,25 @@
+# hugo's code
+
+def encoder(user_input):
+    result = ""
+    for n in user_input:
+        result += str((int(n) + 3)%10)
+    return result
+
+
+password = ""
+while(True):
+    print(
+'''Menu
+-------------
+1. Encode
+2. Decode
+3. Quit\n''')
+    user_input = int(input("Please enter an option: "))
+    if(user_input == 1):
+        password = encoder(input("Please enter your password to encode: "))
+        print("Your password has been encoded and stored!")
+
+
+
+    
